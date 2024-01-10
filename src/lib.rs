31533432d91b19f30b1c0cd8145a5d8f872fad6d
Ryota Mizumaki <statham.programming@gmail.com>
2024-01-10 12:04:31 +0900

    use pulldown_cmark

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
+use pulldown_cmark::{html, Options, Parser};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-  pub fn alert(s: &str);
+pub fn serialize(html_input: &str) -> String {
+  unimplemented!()
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-  alert(&format!("Hello, {}!", name));
+pub fn deserialize(markdown_input: &str) -> String {
+  let mut options = Options::empty();
+  options.insert(Options::ENABLE_STRIKETHROUGH);
+  let parser = Parser::new_ext(markdown_input, options);
+
+  let mut html_output = String::new();
+  html::push_html(&mut html_output, parser);
+
+  html_output
 }
